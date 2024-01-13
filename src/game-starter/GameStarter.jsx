a7dc8edfd311a99fe26e0805975b0be36aed1207
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2024-01-13 14:40:20 +0100

    amount of players is created dynamically

--- src/game-starter/GameStarter.jsx
+++ src/game-starter/GameStarter.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
 const GameStarter = () => {
+  let amountOfPlayers = 2;
+
+  const createPlayers = (amountOfPlayers) => {
+    let players = [];
+    for (let i = 0; i < amountOfPlayers; i++) {
+      players.push(<input type="text" placeholder="Player Name" key={i} />);
+    }
+    return players;
+  };
   return (
-    <form className="bg-white flex flex-col max-w-xl content-center">
-      <input type="text" placeholder="Player Name" />
-      <input type="text" placeholder="Player Name" />
-      <input type="text" placeholder="Player Name" />
+    <form
+      className="bg-white inline-flex flex-col items-center rounded-xl p-6 gap-y-4"
+      action="#"
+    >
+      {amountOfPlayers > 0 && createPlayers(amountOfPlayers)}
       <button type="button">+ Add New Player</button>
       <input type="submit" value="START GAME" />
-      <small className="inline">Please fill in the names of all players.</small>
+      <small className="text-dcred font-sans">
+        Please fill in the names of all players.
+      </small>
     </form>
   );
 };
